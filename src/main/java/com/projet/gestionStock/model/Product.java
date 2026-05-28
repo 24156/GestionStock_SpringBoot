7135c1a664b4698e8b452fd7635e6d34226e7506
Med Elmostapha <dedijekani@gmail.com>
2026-05-28 20:46:41 +0000

    refactor: optimize Product entity using Lombok annotations and JPA best practices

--- src/main/java/com/projet/gestionStock/model/Product.java
+++ src/main/java/com/projet/gestionStock/model/Product.java
@@ -1,98 +1,45 @@
 package com.projet.gestionStock.model;
 
 import jakarta.persistence.*;
-import java.util.Date;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name="products")
 public class Product {
 
     @Id
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    @Column(nullable = false,length = 50)
     private String name;
-    private double price;
-    private int stock;
 
-    private int minStock;
-    private String categoryId;
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(name = "min_stock")
+    private Integer minStock;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "category_id")
+    private Category category;
+
+    @Column(name = "create_at" , updatable = false)
+    private LocalDateTime createdAt;
 
 
-    private Date createdAt;
-
-
-    
-    public Product() {
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
     }
-    static int idCount=1;
-    public Product(String name, double price, int stock, int minStock, String categoryId) {
-        this.id=idCount++;
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-        this.minStock = minStock;
-        this.categoryId = categoryId;
-        this.createdAt=new Date();
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    public int getMinStock() {
-        return minStock;
-    }
-
-    public void setMinStock(int minStock) {
-        this.minStock = minStock;
-    }
-
-    public String getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(String categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-
-    
 
 }
