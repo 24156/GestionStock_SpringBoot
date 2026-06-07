0e3a77eedc67c254315998a37f24a5b9c8e28a8c
Med Elmostapha <dedijekani@gmail.com>
2026-06-07 20:53:35 +0000

    feat(product): add JPQL query for low stock products in repository

--- src/main/java/com/projet/gestionStock/repository/ProductRepository.java
+++ src/main/java/com/projet/gestionStock/repository/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.projet.gestionStock.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.projet.gestionStock.model.Product;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
+    @Query("SELECT p FROM Product p WHERE p.stock <= p.minStock")
+    List <Product> findLowStockProducts();
+
 }
